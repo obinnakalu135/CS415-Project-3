0682239d429ac3e3dff960abef8d3d20df9a5e44
GGDoctor <127359046+GGDoctor@users.noreply.github.com>
2023-12-04 11:32:48 -0800

    FirstPush
    This is taken from a website to get the idea of BFS down. Need to make changes for the window.

--- Main.py
+++ Main.py
@@ -3,14 +3,68 @@
 
 """
 
+"""
+    Main code taken from GeeksForGeeks
+    Was using it as a reference for a BFS algorithm
+    Need to modify it so we can take an array of vectors and output it using numpy or scripy 
+
+"""
+
+
+import numpy
+#import scripy
+import collections
+
 #Best First Search
 
 
 #Breath First Search
+class Graph:
+    def __init___(self):
+        
+        self.graph=defaultdict(list)
 
-
+    # Function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+ 
+    # Function to print a BFS of graph
+    def BFS(self, s):
+ 
+        # Mark all the vertices as not visited
+        visited = [False] * (max(self.graph) + 1)
+ 
+        # Create a queue for BFS
+        queue = []
+ 
+        # Mark the source node as
+        # visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+ 
+        while queue:
+ 
+            # Dequeue a vertex from
+            # queue and print it
+            s = queue.pop(0)
+            print(s, end=" ")
+ 
+            # Get all adjacent vertices of the
+            # dequeued vertex s.
+            # If an adjacent has not been visited,
+            # then mark it visited and enqueue it
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
 
 def main():
+    #quest = input("Enter the graph you would like to traverse: ")
+
+    #match quest:
+
+  
+    
     print("Hello World")
 
 
