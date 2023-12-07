49dd3d9fde63c35f6975d55a6d8df4090b783417
GGDoctor <127359046+GGDoctor@users.noreply.github.com>
2023-12-06 21:39:59 -0800

    Sudo Code written out

--- Main.py
+++ Main.py
@@ -16,47 +16,68 @@ import numpy
 import collections
 
 #Best First Search
+"""
+def Best_First_Search(I,s,t):
+    #Image
+    #Verticies s,t
+    # s is the start vertex, t is the destination
+    # h is a function as described above
 
+    queue = []
+    q.insert(s)
+    Set visited[v] = false all vertices;
+ visited[s] = true;
+ set d[s] = h[s] and d[u] = MaxInt for all other u.
+ while (Q is not empty and (not visited[t])):
+    u = Q.deleteMin()
+    for each neighbor v of u:
+        if (not visited[v])
+            visited[v] = true
+            set color of v to Green
+            d[v] = d[u] + 1
+            prev[v] = u
+            Q.insert(v, d[v] + h[v])
+        // end-for
+    //end-while 
+
+    v = t
+    while(v != s):
+        #Set s to red 
+        v = prev[v]
+
+    #create output image
+    #output d[t]
+"""
 
 #Breath First Search
-class Graph:
-    def __init___(self):
-        
-        self.graph=defaultdict(list)
+def Breath_First_Search(I,s,t):
+    #Image
+    #Verticies s start,t end
+    # s is the start vertex, t is the destination
+    Q = []
+    Q.insert(s)
+    Visited = [False]
+    Visited[s] = True
+    """
+    while (Q is not empty and (not visited[t])):
+    u = Q.deleteMin()
+    for each neighbor v of u:
+        if (not visited[v])
+            visited[v] = true
+            set color of v to Green
+            d[v] = d[u] + 1
+            prev[v] = u
+            Q.insert(v, d[v] + h[v])
+        // end-for
+    //end-while 
+    """
+    v = t
+    while(v != s):
+        #Set s to red 
+        v = prev[v]
 
-    # Function to add an edge to graph
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
- 
-    # Function to print a BFS of graph
-    def BFS(self, s):
- 
-        # Mark all the vertices as not visited
-        visited = [False] * (max(self.graph) + 1)
- 
-        # Create a queue for BFS
-        queue = []
- 
-        # Mark the source node as
-        # visited and enqueue it
-        queue.append(s)
-        visited[s] = True
- 
-        while queue:
- 
-            # Dequeue a vertex from
-            # queue and print it
-            s = queue.pop(0)
-            print(s, end=" ")
- 
-            # Get all adjacent vertices of the
-            # dequeued vertex s.
-            # If an adjacent has not been visited,
-            # then mark it visited and enqueue it
-            for i in self.graph[s]:
-                if visited[i] == False:
-                    queue.append(i)
-                    visited[i] = True
+    #create output image
+    #output d[t]
 
 def main():
     #quest = input("Enter the graph you would like to traverse: ")
