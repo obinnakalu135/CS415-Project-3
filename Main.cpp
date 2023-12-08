8672402b9f051a2682d4f914c8f34005b012ff41
GGDoctor <127359046+GGDoctor@users.noreply.github.com>
2023-12-08 10:59:49 -0800

    Changed to cpp

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include"EasyBMP.h"
+#include<vector>
+using namespace std;
+
+void Breath_First_Search(int I, int s,int t){
+    int Q[I];
+    
+}
+
+void Best_First_Search(){
+
+}
+
+int main(){
+    std::cout<<"Hello World\n";
+    cout<<"fksdjflajfksdjafljfskf";
+    cout<<"\n"; 
+    return 0;
+}
